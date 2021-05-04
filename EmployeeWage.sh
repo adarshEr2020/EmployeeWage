00bab900121a4e0bd7dfbb5fae76413f9dec2568
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-04 23:45:17 +0530

    calculate employee wage for month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -5,15 +5,16 @@ isPartPresent=2
 EmpWagePerHr=20
 FullDayHr=8
 PartDayHr=8
+WorkingDayPerMonth=20
 TotalSalary=0
 Attendance=$((RANDOM%3))
 case $Attendance in
 	    $isPresent) echo "Employee is Present"
-			TotalSalary=$(($FullDayHr * $EmpWagePerHr))
+			TotalSalary=$(($FullDayHr * $EmpWagePerHr * $WorkingDayPerMonth))
 		   	echo $TotalSalary
 			;;
 	$isPartPresent) echo "Employee is Parttime Present"
-			TotalSalary=$(($FullDayHr * $EmpWagePerHr))
+			TotalSalary=$(($FullDayHr * $EmpWagePerHr * $WorkingDayPerMonth))
         		echo $TotalSalary
 			;;
 		     *) echo "Employee is Absent"
