efe9156c89a2d116dda472806c6f5ac0b1a44d5e
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-04 20:10:18 +0530

    Calculate emmployee wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,9 +4,13 @@ isPresent=1
 EmpWagePerHr=20
 FullDayHr=8
 Attendance=$((RANDOM%2))
+TotalSalary=0
 if [ $isPresent -eq $Attendance ]
 then
 	echo "Employee is Present"
+	TotalSalary=$(($FullDayHr * $EmpWagePerHr))
+	echo "$TotalSalary"
 else
 	echo "Employee is Absent"
+	echo "$TotalSalary"
 fi
