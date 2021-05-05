3e5a89d88f1fde4ffa5a64be01fd02c59aabae82
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-05 10:52:46 +0530

    Calculate monthly employeewage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,20 +4,27 @@ isPresent=1
 isPartPresent=2
 EmpWagePerHr=20
 FullDayHr=8
-PartDayHr=8
-WorkingDayPerMonth=20
+PartDayHr=4
 TotalSalary=0
+MonthlySalary=0
 Attendance=$((RANDOM%3))
 case $Attendance in
-	    $isPresent) echo "Employee is Present"
-			TotalSalary=$(($FullDayHr * $EmpWagePerHr * $WorkingDayPerMonth))
-		   	echo $TotalSalary
+   	    $isPresent) echo "Employee is Present"
 			;;
 	$isPartPresent) echo "Employee is Parttime Present"
-			TotalSalary=$(($FullDayHr * $EmpWagePerHr * $WorkingDayPerMonth))
-        		echo $TotalSalary
 			;;
 		     *) echo "Employee is Absent"
+			;;
+esac
+case $Attendance in
+	    $isPresent)	TotalSalary=$(($TotalSalary + ($EmpWagePerHr*$FullDayHr)))
+			MonthlySalary=$(($TotalSalary*20))
+		   	echo "Monthly salary : $MonthlySalary"
+			;;
+	$isPartPresent)
+			TotalSalary=$(($TotalSalary + ($EmpWagePerHr*$PartDayHr)))
         		echo $TotalSalary
+			;;
+		     *)	echo $TotalSalary
 			;;
 esac
